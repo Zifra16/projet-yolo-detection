9f1c882cd5d1c923f84007244693fe3352932e2f
Zifra16 <122222485+Zifra16@users.noreply.github.com>
2025-11-29 11:02:46 +0100

    Initialisation du projet YOLO

---
+++ README.md
@@ -0,0 +1,2 @@
+# Projet YOLOv8
+Détection d’objets avec YOLOv8 en Python.
---
+++ main.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov8n.pt")
+results = model("image.jpg")
+
+results[0].show()
